9ee80775a0536d6f2f12de3001804353da669bc4
Paul Njuga <paulnjuga@gmail.com>
2023-12-12 02:07:35 +0300

    Add runtime measurement main file

---
+++ 0x01-python_async_function/main-files/2-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
+import sys
+sys.path.append('..')
+
+measure_time = __import__('2-measure_runtime').measure_time
+n = 5
+max_delay = 9
+
+print(measure_time(n, max_delay))
