5edd3a0666b4133fc0c36e8c351c540b8ccc73bd
Paul Njuga <paulnjuga@gmail.com>
2023-12-12 01:47:59 +0300

    Fix: use asyncio.as_completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,14 +11,18 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """List of delays
     """
+    # Create Task objects for each coroutine
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    # If you need results from all coroutines,
+    # and can proceed only when all are complete,
+    # use asyncio.gather()
+    # -------------------------------------------
+    # return await asyncio.gather(*tasks)
+
     # If you want to handle results,
     # as soon as they become available,
     # or have a dynamic set of coroutines,
     # use asyncio.as_completed()
     # -------------------------------------------
-    # If you need results from all coroutines,
-    # and can proceed only when all are complete,
-    # use asyncio.gather()
-    # -------------------------------------------
-    return await asyncio.gather(*tasks)
+    return [await task for task in asyncio.as_completed(tasks)]
