69944c81c49bced3e55384dfb2afe5e7a36aecb8
Paul Njuga <paulnjuga@gmail.com>
2023-12-14 01:51:23 +0300

    Fix: edit for pep8

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Yields a random num btn 0 & 10, 10 times
     """
