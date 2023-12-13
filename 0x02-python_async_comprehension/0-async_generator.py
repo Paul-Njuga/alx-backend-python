1e36c747c97b1dbece4ab6079d8904f9a5b406d4
Paul Njuga <paulnjuga@gmail.com>
2023-12-14 01:47:17 +0300

    Fix: add async gen type annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ import asyncio
 import random
 
 
-async def async_generator():
-    """Yields a random num btn 0 & 10
+async def async_generator() -> Generator[float, None, None]:
+    """Yields a random num btn 0 & 10, 10 times
     """
-    for i in range(10):
-        yield random.random() * 10
+    for _ in range(10):
         await asyncio.sleep(1)
+        yield random.random() * 10
