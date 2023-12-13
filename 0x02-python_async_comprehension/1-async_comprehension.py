fd17b39a790892bc627c94b149d4784a40ccd272
Paul Njuga <paulnjuga@gmail.com>
2023-12-14 02:24:17 +0300

    Add async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+import asyncio
+from typing import List
+
+async_gen = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """List comprehension for an __aniter__
+    """
+    return [i async for i in async_gen()]
