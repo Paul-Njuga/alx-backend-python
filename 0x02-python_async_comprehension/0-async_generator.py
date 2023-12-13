6a7894354a41183de61cab7b8108e46810a23bc9
Paul Njuga <paulnjuga@gmail.com>
2023-12-14 01:42:06 +0300

    Fix: use random() instead of uniform()

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,5 +10,5 @@ async def async_generator():
     """Yields a random num btn 0 & 10
     """
     for i in range(10):
-        yield random.uniform(0, 10)
+        yield random.random() * 10
         await asyncio.sleep(1)
