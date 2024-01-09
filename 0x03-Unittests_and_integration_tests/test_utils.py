a51dd205861dbbbc69cf8f6bceb0497079d53f2c
Paul-Njuga <tyrosavant@gmail.com>
2024-01-09 03:15:35 +0300

    Add unit test file

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Unittest module
+"""
+from parameterized import parameterized
+from utils import access_nested_map
+from unittest import Testcase, mock
+from unittest.mock import patch, Mock
+
+
+class TestAccessNestedMap(TestCase):
+    """Test class for nested map func"""
+    # order of args: map, path, expected_output
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_output):
+        """Test method returns correct output"""
+        output = access_nested_map(map, path)
+        self.assertEqual(output, expected_output)
