1586a4b9417e328230bd46f954175713b7b1aaca
Paul-Njuga <tyrosavant@gmail.com>
2024-01-09 04:20:40 +0300

    Add public repo property mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ import client
 from client import GithubOrgClient
 from parameterized import parameterized
 from unittest import TestCase, mock
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock, PropertyMock
 
 
 class TestGithubOrgClient(TestCase):
@@ -23,4 +23,18 @@ class TestGithubOrgClient(TestCase):
         gc.org()
         mock_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
+            )
+
+    def test_public_repos_url(self) -> None:
+        """Tests the `_public_repos_url` property"""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
             )
